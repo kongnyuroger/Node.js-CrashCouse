78b5bcd6b54da87b05bdb9150e11ef2674357b75
Kongnyu Roger <kongnyuroger711@gmail.com>
2023-07-29 10:17:02 +0100

    view engines

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ const express = require('express')
 //express app
 
 const app = express();
+//register view engine
+app.set('view engine', 'ejs')
 
 //listen for requests
 app.listen(3000)
 
-app.get('/', (req, res) => {
-    //res.send('<p>who the hell are you</p>');
-    res.sendFile('./view/index.html', {root: __dirname})
+app.get('/', (req, res) => {const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+
+    res.render('index', {title: 'Home', blogs})
 });
 
 app.get('/about', (req, res) => {
     //res.send('<h1>about page</h1>')
-    res.sendFile('./view/about.html', {root: __dirname})
+    res.render('about', {title: 'About'})
 }) 
 //redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('./about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'create new blog'})
 })
 //404 page
 app.use((req, res) => {
-    res.sendFile('./view/404.html', {root: __dirname})
+    res.render('404', {title: '404'})
 }) 
