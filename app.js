b07eea10962d0ea26c6ed672429d9a769a12bd36
Kongnyu Roger <kongnyuroger711@gmail.com>
2023-08-01 23:41:17 +0100

    conected to mongoDB

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 
 //express app
-
 const app = express();
+//connect to mongoDb
+const dbURI = `mongodb+srv://Roger:Roger@cluster0.xvqg1y0.mongodb.net/node-tut's?retryWrites=true&w=majority`
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(3001))
+.catch((err) => { console.log(err)})
 //register view engine
 app.set('view engine', 'ejs')
 
 //listen for requests
-app.listen(3000)
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
